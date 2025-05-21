796b7af5b6705123997a6e683ede35727fd88572
AbbieF-ctrl <abigail.foggarthy25@compscihigh.org>
2025-05-21 16:02:01 +0000

    Didn't do CSS yet

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const hairSchema = new mongoose.Schema(
         price: {type: Number},
         duration: {type: Number},
         description: {type: String},
-        rating: {type: String}
+        aveRating: {type: String}
     }
 );
 
